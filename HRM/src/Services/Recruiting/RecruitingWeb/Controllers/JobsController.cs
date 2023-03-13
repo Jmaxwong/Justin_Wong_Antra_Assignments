84eea32a3d074125827305bbad46a9712b49d616
Jmaxwong <jmaxwong@gmail.com>
2023-03-13 12:29:25 -0700

    HRM Upload

---
+++ HRM/src/Services/Recruiting/RecruitingWeb/Controllers/JobsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitingWeb.Controllers
+{
+    public class JobsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // return all the jobs so that candidates can apply to the job
+            return View();
+        }
+
+        //get the job detailed information
+        [HttpGet] //this is an attribute
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // Authenticated and User should have role for creating new Job
+        // HR/Manager
+        [HttpPost]
+        public IActionResult Create()
+        {
+            // take the information from the view and save to DB
+            return View();
+        }
+    }
+}
